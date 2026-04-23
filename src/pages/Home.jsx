bd61e382fc425f8e4c6628504e2ab955c401ee66
fcribas <rakky26@gmail.com>
2026-04-22 21:46:08 -0400

    update the typo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Hero from '../components/Hero/Hero.jsx';
 import Services from '../components/Services/Services.jsx';
 import About from '../components/About/About.jsx';
 import Footer from '../components/Footer/Footer.jsx';
-import HowItWorks from '../components/HowItWorks/HowItworks.jsx';
+import HowItWorks from '../components/HowItWorks/HowItWorks.jsx';
 // import BookingSection from '../components/BookingSection/BookingSection.jsx';
 // import ServiceArea from '../components/ServiceArea/ServiceArea.jsx';
 import WhyUs from '../components/WhyUs/WhyUs.jsx';
