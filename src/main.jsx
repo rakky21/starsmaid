b4997043afb6e6e636d6efad6cd201524be73848
fcribas <rakky26@gmail.com>
2026-05-03 14:29:58 -0400

    update

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import App from './App.jsx';
-import './assets/styles/index.css';
+import './index.css';
 import client from './utils/apolloClient.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
